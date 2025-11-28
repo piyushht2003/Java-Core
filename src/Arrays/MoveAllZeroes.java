daf70abeed602e86a42cacae882a2aff78ec6475
Piyush <piyushsinghthakur128@gmail.com>
2025-11-28 20:02:12 +0530

    MoveAllZeroes

---
+++ src/Arrays/MoveAllZeroes.java
@@ -0,0 +1,27 @@
+package Arrays;
+
+public class MoveAllZeroes {
+    public static void print(int arr[]){
+        for(int ele : arr){
+            System.out.print(ele + " ");
+        }
+        System.out.println();
+    }
+    public static void main(String[] args) {
+        int[] arr = {0,1,-2,0,4,7,0,2,8,-1};
+        print(arr);
+        int n = arr.length;
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+
+                // Swap if left element is zero and right is non-zero
+                if (arr[j] == 0 && arr[j + 1] != 0) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+        print(arr);
+    }
+}
